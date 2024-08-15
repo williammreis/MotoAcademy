504b3babf1435bcbed3f4b64b6d5c6cc025eecb7
williammreis <contato.williamandroeza@gmail.com>
2024-08-14 20:10:58 -0400

    Postman

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -4,14 +4,31 @@ const app = express();
 
 app.use(express.json());
 
+const ckeckLogin = (req, res, next) => {
+  const logado = (req.headers.authorization = "Bearer tokenValido");
+  console.log("OI MUNDO");
+
+  if (logado) {
+    next();
+  } else {
+    res.status(401).json({ message: "Por favor verifique suas credencias" });
+  }
+};
+
 let users = [
-  { id: 1, name: "William" },
-  { id: 2, name: "Androeza" },
+  { id: 1, name: "William", email: "william@.com", senha: "12345" },
+  { id: 2, name: "Androeza", email: "androeza@.com", senha: "12345" },
 ];
 
-app.get("/usuarios.com.br", (req, res) => {
-  res.json(users);
+app.get("/usuarios.com.br", ckeckLogin, (req, res) => {
+  console.log("Tchau mundo");
+
+  res.status(201).json(users);
 });
+
+// app.get("/usuarios.com.br", ckeckLogin, (req, res) => {
+//   res.json(users);
+// });
 
 app.post("/usuarios.com.br", (req, res) => {
   const name = req.body.name;
@@ -37,6 +54,16 @@ app.delete("/usuarios.com.br/:id", (req, res) => {
   const id = parseInt(req.params.id);
   users = users.filter((user) => user.id !== id);
   res.sendStatus(204);
+});
+
+app.post("/login", (req, res) => {
+  const { name, email, senha } = req.body;
+  const user = users.find((user) => user.email == email && user.senha == senha);
+  if (user) {
+    res.json({ message: "Login bem sucedido!", token: "tokenValido" });
+  } else {
+    res.json({ message: "Credencias Inválidas" });
+  }
 });
 
 app.get("/", (req, res) => {
